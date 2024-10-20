0d009a37e129582df781dcaf50a785d64e13d2a7
Sigitas Daugnora <sdaugnora@gmail.com>
2024-10-21 00:33:08 +0100

    Setting up game panel for the snake game

---
+++ GamePanel.java
@@ -0,0 +1,21 @@
+public class GamePanel extends JPanel implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    static final int WIDTH = 650;
+    static final int HEIGHT = 650;
+    static final int UNIT_SIZE = 20;
+    static final int NUMBER_OF_UNITS = (WIDTH * HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+
+    final int x[] = new int[NUMBER_OF_UNITS];
+    final int y[] = new int[NUMBER_OF_UNITS];
+
+    int length = 5;
+    int foodEaten;
+    int foodX;
+    int foodY;
+    char direction = 'D';
+    boolean running = false;
+    Random random;
+    Timer timer;
+}
